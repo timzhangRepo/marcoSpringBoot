15a21e832ea38d9d51b09ee5b9a0f52109fc22ba
tim <tim-zhang@live.com>
2024-03-28 23:08:43 -0600

    first commit

---
+++ src/main/java/com/example/marcospringboot/MarcoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.marcospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarcoSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/marcospringboot/MarcoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marcospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
